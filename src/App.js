49d6c3ceb3f63841d712f7355cf7bb62208604cd
Yogurt7v <egorov-2k@yandex.ru>
2024-07-02 16:41:08 +0300

    fix: добавлен хук useViewportSize. добавлен интервал для производительности.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useHover } from './useHover';
+import { useViewportSize } from './useViewportSize';
 import "./App.css";
 
 function App() {
 
-  const { hovered, ref } = useHover();
+  const { height, width } = useViewportSize();
 
     return (
-      <div ref={ref}>
-        {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
-      </div>
+      <>
+      Width: {width}, height: {height}
+    </>
     );
 }
 
---
+++ src/useViewportSize.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+
+export function useViewportSize() {
+  const [size, setSize] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  function changeSize() {
+    setInterval(() => {
+      setSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    }, 500);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", changeSize());
+  }, []);
+
+  return { height: size.height, width: size.width };
+}
